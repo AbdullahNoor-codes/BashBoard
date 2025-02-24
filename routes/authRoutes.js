2300cc72f091b17a599a370713d701a29133365b
Muhammad jahanzaib <126409788+jahanzaib2353@users.noreply.github.com>
2025-02-24 17:38:09 +0500

    Revert "Feature/projects"

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-
-const router = express.Router();
-
-// Login route
-router.post('/', authController.login);
-
-module.exports = router;
